d8d9c87054bc58e8509d0fc57a7c61f8cac82627
Collin Barnwell <collinbarnwell@google.com>
2019-01-24 18:33:11 -0800

    Added clang format on save to init.el

--- init.el
+++ init.el
@@ -1,10 +1,10 @@
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (misterioso)))
+ '(fill-column 100)
  '(inhibit-startup-screen t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -103,3 +103,12 @@
 ;; (setq js-indent-level 2)
 
 (setq indent-tabs-mode nil) ;; tabs are spaces; comment out to set to tabs
+
+(setq-default fill-column 100)
+
+;; sudo apt-get install emacs-google-config
+(require 'google)
+(require 'google-cc-extras)
+(add-hook 'c++-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook #'google-clang-format-file nil :local)))
